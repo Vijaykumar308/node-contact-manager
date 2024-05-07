e68559aef77abd603f7538802559712248f67c6f
vijay kumar <308vijaykumar@gmail.com>
2024-05-07 23:25:15 +0530

    create routes

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("server is running");
+const express = require("express");
+require("dotenv").config();
+
+const contactRouters = require("./routes/contactRoutes");
+
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+// Middleware
+app.use("/api/contacts",contactRouters);
+
+
+
+
+
+
+// Server Setup;
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+});
