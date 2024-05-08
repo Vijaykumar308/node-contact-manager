ef87f23a21a169e5ec8b997dcdce435f44dfe2b6
vijay kumar <308vijaykumar@gmail.com>
2024-05-08 21:58:49 +0530

    created connection with db

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 require("dotenv").config();
-
+const connectDB = require("./configs/dbconn");
 const contactRouters = require("./routes/contactRoutes");
 const errorHandler = require("./middlewares/errorHandler");
 
 const PORT = process.env.PORT || 5001;
+const DATABASE_URL = process.env.DATABASE_URL;
 const app = express();
 
 app.use(express.json());
+
 // Middleware
 app.use("/api/contacts",contactRouters);
 app.use(errorHandler);
 
-
+connectDB(DATABASE_URL);
 
 // Server Setup;
 app.listen(PORT, () => {
